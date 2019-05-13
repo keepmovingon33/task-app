cbfddeda73a534c24aaca988f214fbcc0fec0849
acvan11 <anvanc37@gmail.com>
2019-05-12 19:09:19 -0700

    learning the ObjectID

--- mongodb.js
+++ mongodb.js
@@ -1,15 +1,67 @@
 // CRUD create read update delete
 
-const mongodb = require('mongodb')
-const MongoClient = mongodb.MongoClient
+// const mongodb = require('mongodb')
+// const MongoClient = mongodb.MongoClient
+// const ObjectID = mongodb.ObjectID
+
+const { MongoClient, ObjectID } = require('mongodb')
 
 const connectionURL = 'mongodb://127.0.0.1:/27017'
 const databaseName = 'task-manager'
+
+const id = new ObjectID()
+console.log(id.id.length)
+console.log(id.toHexString())
+// console.log(id.getTimestamp())
 
 MongoClient.connect(connectionURL, { useNewUrlParser: true }, (error, client) => {
 	if (error) {
 		return console.log('Unable to connect to database!')
 	}
 
-	console.log('Connected correctly!!!')
+	// const db = client.db(databaseName)
+	// db.collection('users').insertOne({
+	// 	name: 'Andy',
+	// 	age: 32
+	// }, (error, result) => {
+	// 	if (error) {
+	// 		return console.log('Unable to insert user')
+	// 	}
+	// 	console.log(result.ops)
+	// })
+
+	// db.collection('users').insertMany([
+	// {
+	// 	name: 'Leslie',
+	// 	age: 26
+	// }, {
+	// 	name: 'Phong',
+	// 	age: 33
+	// }
+	// ], (error, result) => {
+	// 	if (error) {
+	// 		return console.log('Unable to insert documents!')
+	// 	}
+
+	// 	console.log(result.ops)
+	// })
+
+	// db.collection('task').insertMany([
+	// {
+	// 	description: 'Finish job1',
+	// 	completed: true
+	// }, {
+	// 	description: 'Finish job2',
+	// 	completed: false
+	// }, {
+	// 	description: 'Finish job3',
+	// 	completed: true
+	// }
+	// 	], (error, result) => {
+	// 		if (error) {
+	// 			return console.log('Unable to insert documents')
+	// 		}
+
+	// 		console.log(result.ops)
+	// 	})
 })
